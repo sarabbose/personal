64d2d9ff690c4f79359b4277271a9a9369fa7aca
joyjit-das <joyjitdas220705@gmail.com>
2025-04-06 18:59:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -635,4 +635,5 @@ if __name__ == "__main__":
         logger.error("Failed to initialize models")
         sys.exit(1)
 
-  
+  port = int(os.environ.get("PORT", 10000))
+     uvicorn.run("main:app", host="0.0.0.0", port=port)
